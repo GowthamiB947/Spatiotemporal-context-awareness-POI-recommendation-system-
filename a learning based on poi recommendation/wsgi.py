a79ac9ea47c61948ee3027169782527743ee0628
Gowthami <gowthamib947@gmail.com>
2026-06-02 16:56:33 +0530

    Add files via upload

---
+++ a learning based on poi recommendation/wsgi.py
@@ -0,0 +1,15 @@
+"""
+WSGI config for a_learning_based_poi_recommendation.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a_learning_based_poi_recommendation.settings')
+application = get_wsgi_application()
